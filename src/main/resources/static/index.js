cfb28333065c1688dfe072f5444f3482a901c5f1
K.Yogiraj <kyogiraj03@gmail.com>
2026-07-18 12:17:03 +0530

    implemented toggleuser

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,2 +1,25 @@
-// alert("hello from js")
-console.log("hello from JS")
+var users=[
+{
+  "name":"Ayanokoji Kiyotaka",
+  "gender": "Sigma male",
+  "image":"ayanokoji.png"
+
+},
+{
+  "name":"Makima",
+  "gender":"Sigma Female",
+  "image":"image.png"
+},
+]
+
+var id=0;
+function toggleuser() {
+   id = (id+1)%2
+
+    var userImage = document.getElementById("user-image");
+      userImage.src = users[id].image;
+    var userName = document.getElementById("user-name");
+       userName.innerHTML = users[id].name;
+    var userGender = document.getElementById("user-gender");
+     userGender.innerHTML = users[id].gender;
+}
